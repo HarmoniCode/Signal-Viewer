0c72e6eeb1bdb1ac4d9c6cff7153d3758b810615
Anasmo1323 <anasmohamed3x2@gmail.com>
2024-10-12 20:03:01 +0300

    Added another plot with the same features separately

--- non_rectangular_withfeatures.py
+++ non_rectangular_withfeatures.py
@@ -10,11 +10,10 @@ from PyQt5.QtCore import QTimer, Qt
 
 class RadarPlotWidget(FigureCanvas):
     def __init__(self, width=5, height=4, dpi=100):
+        # subplot_kw refers to a dictionary contains the names of the available plots where we chose polar here
         fig, self.ax = plt.subplots(figsize=(width, height), dpi=dpi, subplot_kw=dict(polar=True))
         super(RadarPlotWidget, self).__init__(fig)
-        self.ax.set_ylim(0, 5)  # increasing this will add more layers to the polar plot
-        self.categories = []  # Initialize empty categories
-        self.angles = []  # Initialize empty angles
+        self.ax.set_ylim(0, 5)
         self.fill_color = '#b2d1f0'  # Default fill color
         self.line_color = '#287fd5'  # Default line color
 
@@ -26,20 +25,22 @@ class RadarPlotWidget(FigureCanvas):
 
     def update_plot(self, data, categories):
         self.ax.clear()
-        data = np.concatenate((data, [data[0]]))  # Creating a list that starts and ends with the same value
+        data = np.concatenate((data, [data[0]]))  # Closing the radar chart
         # the value of the angle between each category in the plot is from 0 to 360 divided by their number and
         # the endpoint was set to false to prevent the existence of a category 2 times as 0 and 2pi are the same angle
         # .tolist returns the values in a list
-        angles = np.linspace(0, 2 * np.pi, len(categories), endpoint=False).tolist()
-        angles += angles[:1]  # We do this so our array starts and ends with the same value
 
-        # Plotting the data with fill and line
-        self.ax.fill(angles, data, color=self.fill_color, alpha=0.5)
-        self.ax.plot(angles, data, color=self.line_color, linewidth=1)
-        self.ax.set_xticks(angles[:-1])
+        self.angles = np.linspace(0, 2 * np.pi, len(categories), endpoint=False).tolist()
+        self.angles += self.angles[:1]  # We concatenate so our array starts and ends with the same value
+
+        # Plot fill and line
+        self.ax.fill(self.angles, data, color=self.fill_color, alpha=0.5)
+        self.ax.plot(self.angles, data, color=self.line_color, linewidth=1)
+
+        self.ax.set_xticks(self.angles[:-1])
         self.ax.set_xticklabels(categories)
-
-        for i, (angle, value) in enumerate(zip(angles, data)):
+        for i, (angle, value) in enumerate(zip(self.angles, data)):
+            # ax.text puts a label where the plot is with the current value
             self.ax.text(angle, value + 5, f'{value:.2f}', horizontalalignment='center', size=10, color='black',
                          backgroundcolor=self.fill_color)
 
@@ -50,199 +51,192 @@ class RadarPlotApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
-        self.fill_color = '#b2d1f0'  # Default fill color
-        self.line_color = '#287fd5'  # Default line color
-        self.timer = QTimer(self)
-        self.timer.timeout.connect(self.transition_plot)
 
     def initUI(self):
-        self.setWindowTitle('Radar Plot Viewer')
+        self.setWindowTitle('Dual Radar Plot Viewer')
 
-        self.radar_plot = RadarPlotWidget()
+        self.radar_plot1 = RadarPlotWidget()
+        self.radar_plot2 = RadarPlotWidget()
 
-        # Upload CSV button
-        self.upload_button = QPushButton('Upload')
-        self.upload_button.clicked.connect(self.load_csv)
+        self.timer1 = QTimer(self)
+        self.timer1.timeout.connect(lambda: self.transition_plot(self.radar_plot1, 1))
+        self.timer2 = QTimer(self)
+        self.timer2.timeout.connect(lambda: self.transition_plot(self.radar_plot2, 2))
 
-        # Color chooser buttons
-        self.fill_color_button = QPushButton('Choose Fill Color')
-        self.fill_color_button.clicked.connect(self.choose_fill_color)
-        self.fill_color_button.setEnabled(False)
+        self.init_plot_vars(1)
+        self.init_plot_vars(2)
 
-        self.line_color_button = QPushButton('Choose Line Color')
-        self.line_color_button.clicked.connect(self.choose_line_color)
-        self.line_color_button.setEnabled(False)
+        radar_layout = QHBoxLayout()
+        radar_layout.addWidget(self.radar_plot1)
+        radar_layout.addWidget(self.radar_plot2)
 
-        main_widget = QWidget()
-        main_layout = QVBoxLayout(main_widget)
+        controls1 = self.create_controls(self.radar_plot1, self.timer1, plot_num=1)
 
-        main_layout.addWidget(self.upload_button)
-        main_layout.addWidget(self.fill_color_button)
-        main_layout.addWidget(self.line_color_button)
-        main_layout.addWidget(self.radar_plot)
+        controls2 = self.create_controls(self.radar_plot2, self.timer2, plot_num=2)
 
-        control_layout = QHBoxLayout()
+        main_layout = QVBoxLayout()
+        main_layout.addLayout(radar_layout)
+        main_layout.addLayout(controls1)
+        main_layout.addLayout(controls2)
 
-        self.backward_button = QPushButton('Backward')
-        self.backward_button.clicked.connect(self.backward_plot)
-        self.backward_button.setEnabled(False)
-        control_layout.addWidget(self.backward_button)
+        container = QWidget()
+        container.setLayout(main_layout)
+        self.setCentralWidget(container)
 
-        self.play_button = QPushButton('Play')
-        self.play_button.clicked.connect(self.play_plot)
-        self.play_button.setEnabled(False)
-        control_layout.addWidget(self.play_button)
+    def init_plot_vars(self, plot_num):
+        setattr(self, f'row_idx{plot_num}', 0)
+        setattr(self, f'interpolation_steps{plot_num}', 30)
+        setattr(self, f'current_step{plot_num}', 0)
+        setattr(self, f'is_playing{plot_num}', False)
+        setattr(self, f'timer_interval{plot_num}', 50)
 
-        self.forward_button = QPushButton('Forward')
-        self.forward_button.clicked.connect(self.forward_plot)
-        self.forward_button.setEnabled(False)
-        control_layout.addWidget(self.forward_button)
+    def create_controls(self, radar_plot, timer, plot_num):
 
-        self.pause_button = QPushButton('Pause')
-        self.pause_button.clicked.connect(self.pause_plot)
-        self.pause_button.setEnabled(False)
-        control_layout.addWidget(self.pause_button)
+        control_layout = QVBoxLayout()
 
-        self.stop_button = QPushButton('Stop')
-        self.stop_button.clicked.connect(self.stop_plot)
-        self.stop_button.setEnabled(False)
-        control_layout.addWidget(self.stop_button)
+        upload_button = QPushButton(f'Upload CSV for Plot {plot_num}')
+        upload_button.clicked.connect(lambda: self.load_csv(radar_plot, plot_num))
+        control_layout.addWidget(upload_button)
 
-        # Current row label
-        self.current_row_label = QLabel("Current Row: N/A")
-        control_layout.addWidget(self.current_row_label)
+        fill_color_button = QPushButton(f'Choose Fill Color for Plot {plot_num}')
+        fill_color_button.clicked.connect(lambda: self.choose_fill_color(radar_plot))
+        control_layout.addWidget(fill_color_button)
+
+        line_color_button = QPushButton(f'Choose Line Color for Plot {plot_num}')
+        line_color_button.clicked.connect(lambda: self.choose_line_color(radar_plot))
+        control_layout.addWidget(line_color_button)
+
+        cine_control_layout = QHBoxLayout()
+        backward_button = QPushButton('Backward')
+        backward_button.clicked.connect(lambda: self.backward_plot(radar_plot, plot_num))
+        cine_control_layout.addWidget(backward_button)
+
+        play_button = QPushButton('Play')
+        play_button.clicked.connect(lambda: self.play_plot(timer, plot_num))
+        cine_control_layout.addWidget(play_button)
+
+        forward_button = QPushButton('Forward')
+        forward_button.clicked.connect(lambda: self.forward_plot(radar_plot, plot_num))
+        cine_control_layout.addWidget(forward_button)
+
+        pause_button = QPushButton('Pause')
+        pause_button.clicked.connect(lambda: self.pause_plot(timer, plot_num))
+        cine_control_layout.addWidget(pause_button)
+
+        stop_button = QPushButton('Stop')
+        stop_button.clicked.connect(lambda: self.stop_plot(radar_plot, plot_num))
+        cine_control_layout.addWidget(stop_button)
+
+        control_layout.addLayout(cine_control_layout)
 
         speed_layout = QHBoxLayout()
-
         minspeed_label = QLabel('4 FPS')
         maxspeed_label = QLabel('80 FPS')
-
-        self.speed_slider = QSlider(Qt.Horizontal)
-        self.speed_slider.setMinimum(10)
-        self.speed_slider.setMaximum(200)
-        self.speed_slider.setValue(150)
-        self.speed_slider.setTickPosition(QSlider.TicksBelow)
-        self.speed_slider.setTickInterval(20)
-        self.speed_slider.setEnabled(False)
-        self.speed_slider.valueChanged.connect(self.change_speed)
-
+        speed_slider = QSlider(Qt.Horizontal)
+        speed_slider.setMinimum(10)
+        speed_slider.setMaximum(200)
+        speed_slider.setValue(150)
+        speed_slider.setTickPosition(QSlider.TicksBelow)
+        speed_slider.setTickInterval(20)
+        speed_slider.valueChanged.connect(lambda: self.change_speed(timer, plot_num, speed_slider))
         speed_layout.addWidget(minspeed_label)
-        speed_layout.addWidget(self.speed_slider)
+        speed_layout.addWidget(speed_slider)
         speed_layout.addWidget(maxspeed_label)
-
         control_layout.addLayout(speed_layout)
 
-        # Add control layout to the main layout
-        main_layout.addLayout(control_layout)
-        self.setCentralWidget(main_widget)
+        return control_layout
 
-    def choose_fill_color(self):
+    def choose_fill_color(self, radar_plot):
         color = QColorDialog.getColor()
         if color.isValid():
-            self.fill_color = color.name()
+            radar_plot.update_fill_color(color.name())
 
-        self.radar_plot.update_fill_color(self.fill_color)
+    def choose_line_color(self, radar_plot):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            radar_plot.update_line_color(color.name())
 
-    def choose_line_color(self):
-        line_color = QColorDialog.getColor()
-        if line_color.isValid():
-            self.line_color = line_color.name()
-
-        self.radar_plot.update_line_color(self.line_color)
-
-    def load_csv(self):
+    def load_csv(self, radar_plot, plot_num):
         options = QFileDialog.Options()
         file_name, _ = QFileDialog.getOpenFileName(self, "Open CSV File", "", "CSV Files (*.csv);;All Files (*)",
                                                    options=options)
         if file_name:
-            self.csv_data = pd.read_csv(file_name)
-
-            # Ensure that the CSV file has more than 2 columns
-            if self.csv_data.shape[1] < 3:
+            csv_data = pd.read_csv(file_name)
+            if csv_data.shape[1] < 3:
                 raise ValueError("CSV file must contain at least 3 columns.")
 
-            self.play_button.setEnabled(True)
-            self.pause_button.setEnabled(True)
-            self.stop_button.setEnabled(True)
-            self.speed_slider.setEnabled(True)
-            self.backward_button.setEnabled(True)
-            self.forward_button.setEnabled(True)
-            self.line_color_button.setEnabled(True)
-            self.fill_color_button.setEnabled(True)
+            setattr(self, f'csv_data{plot_num}', csv_data)
 
-            # Initialize plot settings
-            self.row_idx = 0
-            self.interpolation_steps = 30
-            self.current_step = 0
-            self.current_data = np.array(self.csv_data.iloc[self.row_idx])
-            self.next_data = np.array(self.csv_data.iloc[(self.row_idx + 1) % len(self.csv_data)])
-            self.is_playing = True  # Default is: not playing
-            self.timer_interval = 50  # Default interval in ms
-            self.timer.start(self.timer_interval)
-            categories = self.csv_data.columns.tolist()  # Get column names as categories
-            self.radar_plot.update_plot(self.current_data, categories)
+            categories = csv_data.columns.tolist()
+            row_idx = getattr(self, f'row_idx{plot_num}')
+            current_data = np.array(csv_data.iloc[row_idx])
+            radar_plot.update_plot(current_data, categories)
 
-            # Call this function to update the row label according to the current data
-            self.update_current_row_label()
+    def change_speed(self, timer, plot_num, slider):
+        speed_value = slider.value()
+        timer_interval = 200 - speed_value
+        setattr(self, f'timer_interval{plot_num}', timer_interval)
+        if getattr(self, f'is_playing{plot_num}'):
+            timer.start(timer_interval)
 
-    def update_current_row_label(self):
-        self.current_row_label.setText(f"Current Row: {self.row_idx + 1}")
+    def play_plot(self, timer, plot_num):
+        setattr(self, f'is_playing{plot_num}', True)
+        timer.start(getattr(self, f'timer_interval{plot_num}'))
 
-    def play_plot(self):
-        self.is_playing = True
-        self.timer.start(self.timer_interval)
+    def pause_plot(self, timer, plot_num):
+        setattr(self, f'is_playing{plot_num}', False)
+        timer.stop()
 
-    def backward_plot(self):
-        self.row_idx = max(0, self.row_idx - 1)
-        self.current_data = np.array(self.csv_data.iloc[self.row_idx])
-        categories = self.csv_data.columns.tolist()
-        self.radar_plot.update_plot(self.current_data, categories)
-        self.update_current_row_label()
+    def stop_plot(self, radar_plot, plot_num):
+        timer = getattr(self, f'timer{plot_num}')
+        timer.stop()
+        self.init_plot_vars(plot_num)
+        radar_plot.ax.clear()
+        radar_plot.draw()
 
-    def forward_plot(self):
-        self.row_idx = (self.row_idx + 1) % len(self.csv_data)
-        self.current_data = np.array(self.csv_data.iloc[self.row_idx])
-        categories = self.csv_data.columns.tolist()
-        self.radar_plot.update_plot(self.current_data, categories)
-        self.update_current_row_label()
+    def backward_plot(self, radar_plot, plot_num):
+        row_idx = getattr(self, f'row_idx{plot_num}')
+        csv_data = getattr(self, f'csv_data{plot_num}')
+        if row_idx > 0:
+            row_idx -= 1
+        setattr(self, f'row_idx{plot_num}', row_idx)
+        current_data = np.array(csv_data.iloc[row_idx])
+        categories = csv_data.columns.tolist()
+        radar_plot.update_plot(current_data, categories)
 
-    def pause_plot(self):
-        self.is_playing = False
-        self.timer.stop()
+    def forward_plot(self, radar_plot, plot_num):
+        row_idx = getattr(self, f'row_idx{plot_num}')
+        csv_data = getattr(self, f'csv_data{plot_num}')
+        row_idx = (row_idx + 1) % len(csv_data)
+        setattr(self, f'row_idx{plot_num}', row_idx)
+        current_data = np.array(csv_data.iloc[row_idx])
+        categories = csv_data.columns.tolist()
+        radar_plot.update_plot(current_data, categories)
 
-    def stop_plot(self):
-        self.is_playing = False
-        self.timer.stop()
-        self.row_idx = 0
-        self.current_step = 0
-        self.current_data = np.array(self.csv_data.iloc[self.row_idx])
-        categories = self.csv_data.columns.tolist()
-        self.radar_plot.update_plot(self.current_data, categories)
-        self.update_current_row_label()
-
-    def change_speed(self):
-        speed_value = self.speed_slider.value()  # Get speed value from the slider
-        self.timer_interval = 200 - speed_value  # Since the slider value is inversely proportional with the time interval
-        if self.is_playing:
-            self.timer.start(self.timer_interval)  # Update the timer if playing
-
-    def interpolate_data(self):
-        factor = self.current_step / self.interpolation_steps
-        interpolated_data = (1 - factor) * self.current_data + factor * self.next_data
+    def interpolate_data(self, current_data, next_data, current_step, interpolation_steps):
+        factor = current_step / interpolation_steps
+        interpolated_data = (1 - factor) * current_data + factor * next_data
         return interpolated_data
 
-    def transition_plot(self):
-        if self.current_step < self.interpolation_steps:
-            interpolated_data = self.interpolate_data()
-            categories = self.csv_data.columns.tolist()
-            self.radar_plot.update_plot(interpolated_data, categories)
-            self.current_step += 1
+    def transition_plot(self, radar_plot, plot_num):
+        current_step = getattr(self, f'current_step{plot_num}')
+        interpolation_steps = getattr(self, f'interpolation_steps{plot_num}')
+        row_idx = getattr(self, f'row_idx{plot_num}')
+        csv_data = getattr(self, f'csv_data{plot_num}')
+        categories = csv_data.columns.tolist()
+
+        current_data = np.array(csv_data.iloc[row_idx])
+        next_data = np.array(csv_data.iloc[(row_idx + 1) % len(csv_data)])
+
+        interpolated_data = self.interpolate_data(current_data, next_data, current_step, interpolation_steps)
+        radar_plot.update_plot(interpolated_data, categories)
+
+        if current_step < interpolation_steps:
+            current_step += 1
+            setattr(self, f'current_step{plot_num}', current_step)
         else:
-            self.row_idx = (self.row_idx + 1) % len(self.csv_data)
-            self.current_data = np.array(self.csv_data.iloc[self.row_idx])
-            self.next_data = np.array(self.csv_data.iloc[(self.row_idx + 1) % len(self.csv_data)])
-            self.current_step = 0
-            self.update_current_row_label()
+            setattr(self, f'row_idx{plot_num}', (row_idx + 1) % len(csv_data))
+            setattr(self, f'current_step{plot_num}', 0)
 
 
 if __name__ == '__main__':
